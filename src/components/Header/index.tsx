c67b3f120e6dc78a59389729d287defe099bb1bc
Lucas Nunes <ns_lucas@hotmail.com>
2023-06-13 20:58:37 -0300

    Listening more webhooks

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,18 +2,20 @@ import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Header() {
-	return (
-		<header className={styles.headerContainer}>
-			<div className={styles.headerContent}>
-				<img src="/images/logo.svg" alt="ig.news" />
-				<nav>
-					<a className={styles.active} href="#">
-						Home
-					</a>
-					<a href="#">Posts</a>
-				</nav>
-				<SignInButton />
-			</div>
-		</header>
-	);
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <a href="/">
+          <img src="/images/logo.svg" alt="ig.news" />
+        </a>
+        <nav>
+          <a className={styles.active} href="#">
+            Home
+          </a>
+          <a href="#">Posts</a>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
