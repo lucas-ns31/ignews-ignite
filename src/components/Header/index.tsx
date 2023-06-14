66f6b171e71bc1edbfaffcb6a0a707c49534d419
Lucas Nunes <ns_lucas@hotmail.com>
2023-06-13 22:08:37 -0300

    Configuring prismic to create posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,9 +5,7 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <a href="/">
-          <img src="/images/logo.svg" alt="ig.news" />
-        </a>
+        <img src="/images/logo.svg" alt="ig.news" />
         <nav>
           <a className={styles.active} href="#">
             Home
